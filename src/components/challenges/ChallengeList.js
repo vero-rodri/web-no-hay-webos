5ad70105d3f00be0d060ba2d2480ec16e5b39aa2
Rodrigo Otero <roterom82@gmail.com>
2019-03-29 20:41:06 +0100

    feat:searchBar done

--- src/components/challenges/ChallengeList.js
+++ src/components/challenges/ChallengeList.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import challengesService from '../../services/challengesService';
 import ChallengeItem from './ChallengeItem';
+import SearchBar from '../../ui/searchBar';
 
 class ChallengeList extends Component {
   state = {
-    challenges: []
+    challenges: [],
+    search: ''
   }
 
   componentDidMount = () => {
@@ -15,12 +17,20 @@ class ChallengeList extends Component {
   }
 
   challengeList = () => 
-    this.state.challenges.map(challenge => <ChallengeItem key={challenge.id} {...challenge} />)
-  
-  
+    this.state.challenges
+      .filter(challenge => challenge.title.includes(this.state.search))
+      .map(challenge => <ChallengeItem key={challenge.id} {...challenge} />)
+      //(challenge.title.includes(this.state.search) && <ChallengeItem key={challenge.id} {...challenge} />))
+      //<ChallengeItem key={challenge.id} {...challenge} />)
+
+  handleSearch = (keyword) => 
+    this.setState({...this.state,
+                  search: keyword});
+
   render() {
     return (
       <div>
+        <SearchBar search={this.state.search} setSearch={this.handleSearch}/>
         {this.challengeList()}
       </div>
     )
