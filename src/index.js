a790c74b776d0ee6cc027af835c5f4b3a829f14a
Vero Celemin <veronica.celemin@gmail.com>
2019-04-11 12:43:00 +0200

    fix: styles added

--- src/index.js
+++ src/index.js
@@ -9,20 +9,27 @@ import * as serviceWorker from './serviceWorker';
 import { Grommet } from 'grommet';
 import customTheme from './utils/theme-grommet'
 import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
 import 'moment-timezone';
 import { SearchStore } from '../src/context/SearchStore';
 import { AuthStore } from './context/AuthStore';
 
 
 ReactDOM.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
-    <AuthStore>
-      <SearchStore>
-        <Grommet  theme={customTheme}>
-          <App />
-        </Grommet>
-      </SearchStore>
-    </AuthStore>
-  </HashRouter>,
+  // <HashRouter>
+     <BrowserRouter >
+      <AuthStore>
+        <SearchStore>
+          <Grommet  theme={customTheme}>
+            <App />
+          </Grommet>
+        </SearchStore>
+      </AuthStore>
+    </BrowserRouter>,
+  // </HashRouter>,
  document.getElementById('root'));
 serviceWorker.unregister();
+
+
+{/* <HashRouter basename={process.env.PUBLIC_URL}>
+ */}
